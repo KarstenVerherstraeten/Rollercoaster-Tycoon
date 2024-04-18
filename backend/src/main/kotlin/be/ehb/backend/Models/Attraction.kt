6a5042e9e3946e367a9a1f6b2555a4d57da87d37
Karsten Verherstraeten <karsten.verherstraeten@student.ehb.be>
2024-04-18 16:16:42 +0200

    changes to the Attraction model
    made some ellements nullable for testing purpose so i can more easily create attractions in test fase via site

--- backend/src/main/kotlin/be/ehb/backend/Models/Attraction.kt
+++ backend/src/main/kotlin/be/ehb/backend/Models/Attraction.kt
@@ -8,20 +8,23 @@ import java.util.*
 @Table(name = "attractions")
 
 data class Attraction (
+
+    // vergeet niet date etc terug niet nullable te maken
+
     @Id
     @GeneratedValue
     var id: Long = -1,
     val name: String,
     val buildYear: Int,
     val capacity: Int,
-    val picture: String,
-    val video: String,
+    val picture: String?,
+    val video: String?,
     val minHeight: Int,
     val maxHeight: Int,
     val fastPass: Boolean,
     val disabled: Boolean,
     val description: String,
-    val maintenancePeriod: Date
+    val maintenancePeriod: Date?
 
 
 ) {
