de6d82001fd645a4a8f5d6164279ca30eea5527d
Karsten Verherstraeten <karsten.verherstraeten@student.ehb.be>
2024-05-23 15:07:02 +0200

    expanded the editable fields

--- frontend/CourseProject/src/components/Attractions/EditAttraction.vue
+++ frontend/CourseProject/src/components/Attractions/EditAttraction.vue
@@ -9,13 +9,17 @@ export default {
 		return {
 			editedName: "",
 			editedDescription: "",
+			editedBuildyear: "",
+			editedCapacity: "",
+			editedMinHeight: "",
+			editedMaxHeight: "",
 			isEditMode: false,
 			errorMessage: "",
 		};
 	},
 	methods: {
 		editAttraction(id) {
-			if (this.editedName && this.editedDescription) {
+			if (this.editedName && this.editedDescription && this.editedBuildyear && this.editedCapacity && this.editedMinHeight && this.editedMaxHeight) {
 				fetch(`http://localhost:1234/attractions/${id}`, {
 					method: "PUT",
 					body: JSON.stringify({
@@ -26,11 +30,11 @@ export default {
 						"Content-Type": "application/json",
 					},
 				}).then(() => {
-					this.$emit("attraction-edited", { id: id, name: this.editedName, description: this.editedDescription });
+					this.$emit("attraction-edited", { id: id, name: this.editedName, description: this.editedDescription, buildyear: this.editedBuildyear, capacity: this.editedCapacity, minHeight: this.editedMinHeight, maxHeight: this.editedMaxHeight, fastpass: this.fastpass});
 					this.$router.go();
 				});
 			} else {
-				this.errorMessage = "Please fill in all fields.";
+				this.errorMessage = "Please fill in all fields";
 			}
 		},
 		enterEditMode() {
@@ -38,12 +42,20 @@ export default {
 			this.isEditMode = true;
 			this.editedName = "";
 			this.editedDescription = "";
+			this.editedBuildyear = "";
+			this.editedCapacity = "";
+			this.editedMinHeight = "";
+			this.editedMaxHeight = "";
 			this.errorMessage = "";
 		},
 		cancelEditMode() {
 			this.isEditMode = false;
 			this.editedName = this.name; // Reset editedName to the original name
 			this.editedDescription = this.description; // Reset editedDescription to the original description
+			this.editedBuildyear = this.buildyear;
+			this.editedCapacity = this.capacity;
+			this.editedMinHeight = this.minHeight;
+			this.editedMaxHeight = this.maxHeight;
 			this.errorMessage = "";
 		},
 	},
@@ -56,6 +68,10 @@ export default {
 			<div>
 				<input v-model="editedName" placeholder="Naam:" type="text" />
 				<input v-model="editedDescription" placeholder="Beschrijving:" type="text" />
+				<input v-model="editedBuildyear" placeholder="Bouwjaar:" type="number" />
+				<input v-model="editedCapacity" placeholder="Capaciteit:" type="number" />
+				<input v-model="editedMinHeight" placeholder="Minimale lengte:" type="number" />
+				<input v-model="editedMaxHeight" placeholder="Maximale lengte:" type="number" />
 			</div>
 			<div>
 				<button @click="editAttraction(id)">Save</button>
